32aa05d4973d8dad491a6abd176bdeedbbed97cd
Adi <98210910+adityas-github@users.noreply.github.com>
2024-10-21 10:31:25 +0530

    Time: 227 ms (59.06%), Space: 47.8 MB (66.67%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    unordered_set<string> st;
-    int maxUniqueSplit(string& s, int p = 0) {
-        if (p == s.size())
-            return 0;
-        int res = -1;
-        for (int sz = 1; p + sz <= s.size(); ++sz) {
-            auto it = st.insert(s.substr(p, sz));
-            if (it.second) {
-                int n_res = maxUniqueSplit(s, p + sz);
-                if (n_res != -1)
-                    res = max(res, 1 + n_res);
-                st.erase(it.first);
+    int maxUniqueSplit(string s) {
+        unordered_set<string> seen;
+        return backtrack(s, 0, seen);
+    }
+
+private:
+    int backtrack(const string& s, int start, unordered_set<string>& seen) {
+        // Base case: If we reach the end of the string, return 0 (no more
+        // substrings to add)
+        if (start == s.size()) return 0;
+
+        int maxCount = 0;
+
+        // Try every possible substring starting from 'start'
+        for (int end = start + 1; end <= s.size(); ++end) {
+            string substring = s.substr(start, end - start);
+            // If the substring is unique
+            if (seen.find(substring) == seen.end()) {
+                // Add the substring to the seen set
+                seen.insert(substring);
+                // Recursively count unique substrings from the next position
+                maxCount = max(maxCount, 1 + backtrack(s, end, seen));
+                // Backtrack: remove the substring from the seen set
+                seen.erase(substring);
             }
         }
-        return res;
+        return maxCount;
     }
 };
