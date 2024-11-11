b29ddb9c841200621564f73b182ead1df05a7355
Adi <98210910+adityas-github@users.noreply.github.com>
2024-11-11 07:13:44 +0530

    Time: 8 ms (34.71%), Space: 30.4 MB (12.46%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    void seive_algo(vector<bool> &v, int n){
-        v[0] = v[1] = false;
-        for(int i=2; i*i<n; i++){
-            for(int j=2*i; j<n; j+=i){
-                v[j] = false;
-            }
-        }
-    }
-
     bool primeSubOperation(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> isPrime(1005, true);
-        seive_algo(isPrime, 1005);
-
-        vector<int> prime;
-        for(int i=0; i<1004; i++)
-            if(isPrime[i]) prime.push_back(i);
-        
-        int flg = 0;
-        int prev = nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            if(nums[i]<prev){prev = nums[i]; continue;}
-            flg = 1;
-            for(int sub=0; sub<prime.size() && prime[sub]<nums[i]; sub++){
-                if(nums[i]-prime[sub]<prev){
-                    prev = nums[i]-prime[sub];
-                    flg = 0;
-                    break;
+        vector<int> prime(1000,true);
+        for(int i=2;i *i<1000;i++)
+        {
+            if(prime[i])
+            {
+                for(int j=2;j*i<1000;j++)
+                {
+                    prime[j*i]=false;
                 }
-            }
-            if(flg) break;
-
+            } 
         }
-        return !flg;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            continue;
+            else
+            {
+                bool flag=true;
+                for(int j=2;j<nums[i];j++)
+                {
+                    if(prime[j] && (nums[i]-j)<nums[i+1])
+                    {
+                        nums[i]=nums[i]-j;
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag)
+                return false;
+
+            }
+        }
+        return true;
     }
 };
