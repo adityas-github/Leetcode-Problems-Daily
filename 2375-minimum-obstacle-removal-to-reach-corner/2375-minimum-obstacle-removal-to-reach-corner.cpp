472912c48495696a76a846f2fe71088a4da6d73e
Adi <98210910+adityas-github@users.noreply.github.com>
2024-11-28 07:41:09 +0530

    Time: 80 ms (99.69%), Space: 107.6 MB (91.02%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int minimumObstacles(vector<vector<int>>& mat) {
-         vector<pair<int,int>>d={{-1,0},{0,1},{1,0},{0,-1}};
-        int n=mat.size();
-        int m=mat[0].size();
-        queue<pair<int,int>>q;
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int n = grid.size(),m=grid[0].size();
+        int dp[n][m];
+        memset(dp,-1,sizeof(dp));
+        dp[0][0] = grid[0][0];
+        int dx[] ={1,-1,0,0};
+        int dy[] ={0,0,1,-1};
+        queue <pair<int,int>> q,q1;
         q.push({0,0});
-        vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
-        dp[0][0]=0;
-        while(q.size())
-        {
-            int size=q.size();
-            for(int i=0;i<size;i++)
-            {
-                auto temp=q.front();
-                q.pop();
-                int x=temp.first;
-                int y=temp.second;
-                for(auto it:d)
-                {
-                    int nx=x+it.first;
-                    int ny=y+it.second;
-                    if(nx>=0&&ny>=0&&nx<n&&ny<m&&dp[x][y]+mat[nx][ny]<dp[nx][ny])
+        while(!q.empty()){
+            pair<int,int> p = q.front();
+            q.pop();
+            for(int i=0;i<4;i++){
+                int x = p.first+dx[i],y=p.second+dy[i];
+                if(x>=0&&x<n&&y>=0&&y<m&&dp[x][y]==-1){
+                    dp[x][y] = dp[p.first][p.second] + grid[x][y];
+                    if(grid[x][y]){
+                        q1.push({x,y});
+                    }
+                    else
                     {
-                        dp[nx][ny]=dp[x][y]+mat[nx][ny];
-                         q.push({nx,ny});
+                        q.push({x,y});
                     }
                 }
             }
+            if(q.empty()){
+                swap(q,q1);
+            }
         }
         return dp[n-1][m-1];
-                
     }
 };
