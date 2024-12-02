6d65284e5fdc940dfcafae36a91c49c472623450
Adi <98210910+adityas-github@users.noreply.github.com>
2024-12-02 09:54:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (8.49%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
-        auto sent = " " + sentence, word = " " + searchWord;
-        int word_cnt = 0, j = 0;
-        for (auto i = 0; i < sent.size() && j < word.size(); ++i) {
-            word_cnt += sent[i] == ' ';
-            if (sent[i] == word[j])
-                ++j;
-            else
-                j = sent[i] == word[0] ? 1 : 0;
-        }
-        return j == word.size() ? word_cnt : -1;
+        string str = " " + searchWord;
+        if (sentence.substr(0, searchWord.length()) == searchWord)
+            return 1;
+        if (sentence.contains(str))
+            return count(sentence.begin(),
+                         sentence.begin() + sentence.find(str), ' ') +
+                   2;
+        return -1;
     }
 };
