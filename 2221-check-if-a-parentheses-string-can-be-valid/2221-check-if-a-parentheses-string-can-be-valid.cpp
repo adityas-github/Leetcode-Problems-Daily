d1904bf78f8f9641f7155433f946f0953a640b19
Adi <98210910+adityas-github@users.noreply.github.com>
2025-01-12 17:11:23 +0530

    Time: 14 ms (55.08%), Space: 30.1 MB (53.05%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        int length = s.size();
-        // If length of string is odd, return false.
-        if (length % 2 == 1) {
-            return false;
-        }
-
-        stack<int> openBrackets, unlocked;
-
-        // Iterate through the string to handle '(' and ')'
-        for (int i = 0; i < length; i++) {
-            if (locked[i] == '0') {
-                unlocked.push(i);
-            } else if (s[i] == '(') {
-                openBrackets.push(i);
-            } else if (s[i] == ')') {
-                if (!openBrackets.empty()) {
-                    openBrackets.pop();
-                } else if (!unlocked.empty()) {
-                    unlocked.pop();
-                } else {
-                    return false;
-                }
-            }
-        }
-
-        // Match remaining open brackets with unlocked characters
-        while (!openBrackets.empty() && !unlocked.empty() &&
-               openBrackets.top() < unlocked.top()) {
-            openBrackets.pop();
-            unlocked.pop();
-        }
-
-        if (!openBrackets.empty()) {
-            return false;
-        }
-
+         int n = s.length(), i, l = 0, u = 0;
+        if(n & 1)   return false;
+        for(i = 0; i < n; i++)  if(locked[i] == '0')    u++;
+        else if(s[i] == '(')    ++l;
+        else if(l)  --l;
+        else if(u)  --u;
+        else    return false;
+        l = u = 0;
+        for(i = n - 1; i >= 0; --i) if(locked[i] == '0')    u++;
+        else if(s[i] == ')')    ++l;
+        else if(l)  --l;
+        else if(u)  --u;
+        else    return false;
         return true;
     }
 };
