07c86e7767a7b412f6b9b0556b09f4f9982a4c3e
Adi <98210910+adityas-github@users.noreply.github.com>
2025-05-11 16:26:36 +0530

    Time: 0 ms (100%), Space: 8.8 MB (99.95%) - LeetHub

--- 3871-minimum-deletions-for-at-most-k-distinct-characters/3871-minimum-deletions-for-at-most-k-distinct-characters.cpp
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/3871-minimum-deletions-for-at-most-k-distinct-characters.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
+      int cnt[26];
     int minDeletion(string s, int k) {
-       map<char,int>m;
-       for(int i=0;i<s.length();i++){
-        m[s[i]]++;
-       } 
-       vector<int>v;
-       for(auto i:m){
-        v.push_back(i.second);
-       }
-       sort(v.begin(),v.end());
-       int si=v.size();
-       int d = si-k;
-       if(d<=0){
-        return 0;
-       }
-       else{
-        int a = 0,i=0;
-        while(d--){
-            a+=v[i];
-            i++;
+        for(auto &c:s)cnt[c-'a']++;
+    
+        sort(cnt,cnt+26,greater<int>());
+        
+        int ans=0;
+        for(int a=0;a<26;a++){
+            if(a>=k)ans+=cnt[a];
         }
-        return a;
-       }
+
+        return ans;
     }
 };
