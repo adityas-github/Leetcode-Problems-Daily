53510e39c3177bcd6716310f356541fca69798a5
Adi <98210910+adityas-github@users.noreply.github.com>
2025-05-22 19:59:01 +0530

    Time: 0 ms (100%), Space: 9.7 MB (65.26%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        map<char, int> v, c;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+                v[s[i]]++;
+            }
+            else{
+                c[s[i]]++;
+            }
+            
+        }
+        int m1=0,m2=0;
+
+        for(auto i:v){
+            m1=max(m1,i.second);
+        }
+         for(auto i:c){
+            m2=max(m2,i.second);
+        }
+return m1+m2;
+    }
+};
