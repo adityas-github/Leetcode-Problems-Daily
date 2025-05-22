8f4215847e209037081dbf6f4e39536432ffd592
Adi <98210910+adityas-github@users.noreply.github.com>
2025-05-22 19:59:26 +0530

    Time: 2 ms (43.84%), Space: 9.9 MB (45.36%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cpp
@@ -3,22 +3,21 @@ public:
     int maxFreqSum(string s) {
         map<char, int> v, c;
         for (int i = 0; i < s.length(); i++) {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+                s[i] == 'u') {
                 v[s[i]]++;
-            }
-            else{
+            } else {
                 c[s[i]]++;
             }
-            
         }
-        int m1=0,m2=0;
+        int m1 = 0, m2 = 0;
 
-        for(auto i:v){
-            m1=max(m1,i.second);
+        for (auto i : v) {
+            m1 = max(m1, i.second);
         }
-         for(auto i:c){
-            m2=max(m2,i.second);
+        for (auto i : c) {
+            m2 = max(m2, i.second);
         }
-return m1+m2;
+        return m1 + m2;
     }
 };
